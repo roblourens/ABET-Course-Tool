35d56bcb4abc1d167076b25f893bf2e1813ea0d4
Ryan Sanders <rsanders16@gmail.com>
2011-10-17 01:32:17 -0500

    Made no changes

--- prototype/scripts/php/ajax.saveCourse.php
+++ prototype/scripts/php/ajax.saveCourse.php
@@ -1,6 +1,7 @@
 <?php 
 if(!isset($_POST['json']))die("ERROR:  No data was sent from the client");
 $data = json_decode($_POST['json'], true);
+print_r($data);
 $course_file = "../../../data/courses/".$data['course_named']."/".$data['course_named'].".json";
 if(!file_exists($course_file))die("ERROR: The course ".$data['course_named']." does not exist.");
 $fh = fopen($course_file, 'w') or die("ERROR: The course data for '".$data['course_named']."' could not be saved.");
