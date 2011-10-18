df37d7d000467910e58c5855a3aa61a2e9a9661f
rob <roblourens@gmail.com>
2011-10-17 20:14:35 -0500

    Modified include.php

--- src/include.php
+++ src/include.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once('./model/data.php');
-require_once('./model/Course.php');
-require_once('./lib/jsonschemaphp/jsonSchema.php');
+$ROOT = '/Users/rob/Sites/ABET-Course-Tool/';
+require_once($ROOT.'src/model/data.php');
+require_once($ROOT.'src/model/Course.php');
+require_once($ROOT.'lib/jsonschemaphp/jsonSchema.php');
 
 ?>
