9276ed571680859e3d6d463ac30c7b91e2d5f37d
rob <roblourens@gmail.com>
2011-12-11 23:20:35 -0600

    Sorts courses on the "view all courses" page so that required courses are listed first
    This could probably be extended to be more easily applied to other
    pages, depending on how we want to do it

--- prototype/viewAllCourses.php
+++ prototype/viewAllCourses.php
@@ -3,6 +3,20 @@ require_once("include/header.php");
 require_once('../src/include.php');
 
 $programs = getPrograms();
+
+
+function compareCourses($c1, $c2, $progID)
+{
+    $course1 = getCourseForID($c1);
+    $course2 = getCourseForID($c2);
+
+    if ($course1->reqForProgram[$progID] == 'R' && $course2->reqForProgram[$progID] != 'R')
+        return -1;
+    else if ($course2->reqForProgram[$progID] == 'R' && $course1->reqForProgram[$progID] != 'R')
+        return 1;
+    else
+        return strcmp($course1->courseID, $course2->courseID);
+}
 ?>
 
 <div class="main">
@@ -15,6 +29,9 @@ $programs = getPrograms();
 foreach ($programs as $prog)
 {
     $progCourses = getCourseIDsForProgramID($prog['short']);
+    $progID = $prog['short'];
+    $compareCoursesF = create_function('$c1,$c2', "return compareCourses(\$c1, \$c2, '$progID');");
+    usort($progCourses, $compareCoursesF);
     echo "<h3 class='prog_name'>".$prog['long']."</h3>";
 	echo "<table>";
     foreach ($progCourses as $courseID)
