c17cc215455025287587ec6ba4d1b337076d3799
rob <roblourens@gmail.com>
2012-03-11 11:25:13 -0500

    didn't work after all...

--- src/include.php
+++ src/include.php
@@ -1,8 +1,8 @@
 <?php
 
 date_default_timezone_set("America/Chicago");
-//$ROOT = '/Users/rob/Sites/ABET-Course-Tool/';
-$ROOT = getcwd()."/../";
+$ROOT = '/Users/rob/Sites/ABET-Course-Tool/';
+//$ROOT = getcwd()."/../";
 require_once($ROOT.'src/model/data.php');
 require_once($ROOT.'src/model/Course.php');
 require_once($ROOT.'src/model/Assignment.php');
